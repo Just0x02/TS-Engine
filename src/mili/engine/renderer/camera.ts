07c9b76abb6947a741078caa91e38397231a39a0
BuildTools <unconfigured@null.spigotmc.org>
2022-06-15 01:51:01 -0400

    Testing out some settings

--- src/mili/engine/renderer/camera.ts
+++ src/mili/engine/renderer/camera.ts
@@ -1,17 +1,21 @@
 import { IObservable } from './observable';
 import { Vec2 } from '../geometry/vec';
+import { Renderer } from '../../renderer';
 
 export class Camera implements IObservable
 {
 	public pos: Vec2;
 	public zoom: number = 1.0;
+	public renderer: Renderer;
 
 	constructor(
 		pos: Vec2,
-		zoom: number = 1.0
+		zoom: number = 1.0,
+		renderer: Renderer
 	)
 	{
 		this.pos = pos;
 		this.zoom = zoom;
+		this.renderer = renderer;
 	}
 }
